f0a4796c7f7e5852a51096c6d02d9e5834118648
ThomasGrandmagnac <thomasgrandmagnac@gmail.com>
2016-05-30 14:25:08 +0200

    Update of MVC POO Exercise

---
+++ admin/index.php
@@ -0,0 +1,26 @@
+<?php
+chdir($appRoot = dirname(__DIR__));
+require_once "init.php";
+// demarre notre application
+$page = new \Controller\PageController($pdo);
+// afficher la page demandee
+$action = 'home';
+// recuperation du slug du parametre d'url si present
+if (isset($_GET['a'])) {
+    $action = $_GET['a'];
+}
+switch($action){
+    case 'ajouter':
+        break;
+    case 'modifier':
+        break;
+    case 'supprimer':
+        break;
+    case 'details':
+        break;
+    case 'lister':
+    default:
+        $page->listeAction();
+        break;
+}
+$page->displayAction();
--- view/nav.php
+++ view/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="/">CMSG1</a>
+            <a class="navbar-brand" href="./">CMS MVC POO</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
